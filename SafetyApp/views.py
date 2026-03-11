d54a5f944a3d178b227b3298c291d351a17dcd26
manu18-m <koonamanoj18@gmail.com>
2026-03-11 23:51:06 +0530

    Changed Views.py

--- SafetyApp/views.py
+++ SafetyApp/views.py
@@ -15,172 +15,225 @@ import smtplib
 from datetime import date
 import seaborn as sns
 
+
 global username, email
-dataset = pd.read_csv("Dataset/CrimesOnWomenData.csv")
-states = np.unique(dataset['State'])
+
+# ✅ DO NOT load dataset at startup
+dataset = None
+states = None
+
+
+def load_dataset():
+    global dataset, states
+    if dataset is None:
+        dataset = pd.read_csv("Dataset/CrimesOnWomenData.csv")
+        states = np.unique(dataset['State'])
+
+
+# ---------------- ROUTE ----------------
 
 def Route(request):
-    if request.method == 'GET':
-        return render(request, 'Route.html', {})
+    return render(request, 'Route.html', {})
+
 
 def RouteAction(request):
     if request.method == 'POST':
         areaname = request.POST.get('t1', False)
-        areaname = areaname+" Police Station"
-        areaname = areaname.replace(" ","+")
-        output = '<iframe width="800" height="650" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q='+areaname+'&amp;ie=UTF8&amp;&amp;output=embed"></iframe><br/>'
-        context= {'data':output}
+        areaname = areaname + " Police Station"
+        areaname = areaname.replace(" ", "+")
+        output = '<iframe width="800" height="650" src="https://maps.google.com/maps?q='+areaname+'&output=embed"></iframe>'
+        context = {'data': output}
         return render(request, 'UserScreen.html', context)
+
+
+# ---------------- HEATMAP ----------------
 
 def Heatmap(request):
-    if request.method == 'GET':
-        global dataset, states
-        heatmap = dataset.groupby('State')['Rape'].sum().sort_values(ascending=False).reset_index(name="Total Crimes")[0:10]
-        labels = heatmap['State'].ravel()
-        heatmap.drop(['State'], axis = 1,inplace=True)
-        heatmap = heatmap.values
-        data = []
-        for i in range(len(heatmap)):
-            values = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            values[i] = heatmap[i,0]
-            data.append(values)
-        heatmap = np.asarray(data)    
-        ax = sns.heatmap(heatmap, xticklabels = labels, yticklabels = labels, annot = True, cmap="viridis" ,fmt ="g");
-        ax.set_ylim([0,len(heatmap)])
-        plt.title("Women Incident Crime Heatmap")
-        buf = io.BytesIO()
-        plt.savefig(buf, format='png', bbox_inches='tight')
-        #plt.close()
-        img_b64 = base64.b64encode(buf.getvalue()).decode()
-        plt.clf()
-        plt.cla()
-        context= {'data':'Women Incident Crime Heatmap', 'img': img_b64}
-        return render(request, 'UserScreen.html', context)
+
+    load_dataset()
+
+    heatmap = dataset.groupby('State')['Rape'].sum().sort_values(ascending=False).reset_index(name="Total Crimes")[0:10]
+
+    labels = heatmap['State'].ravel()
+    heatmap.drop(['State'], axis=1, inplace=True)
+
+    heatmap = heatmap.values
+
+    data = []
+
+    for i in range(len(heatmap)):
+        values = [0]*10
+        values[i] = heatmap[i,0]
+        data.append(values)
+
+    heatmap = np.asarray(data)
+
+    ax = sns.heatmap(heatmap, xticklabels=labels, yticklabels=labels, annot=True)
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    img_b64 = base64.b64encode(buf.getvalue()).decode()
+
+    plt.clf()
+
+    context = {'data': 'Heatmap', 'img': img_b64}
+
+    return render(request, 'UserScreen.html', context)
+
+
+# ---------------- TRAIN ML ----------------
 
 def TrainML(request):
-    if request.method == 'GET':
-        f = open('model/model_history.pckl', 'rb')
-        hist = pickle.load(f)
-        f.close()
-        plt.figure(figsize=(5,3))
-        plt.plot(hist['accuracy'], color = 'blue', label = 'Training Accuracy')
-        plt.plot(hist['val_accuracy'], color = 'green', label = 'Validation Accuracy')
-        plt.title('Neural Network Accuracy Graph')
-        plt.xlabel('Epochs')
-        plt.ylabel('Accuracy')
-        plt.legend()
-        buf = io.BytesIO()
-        plt.savefig(buf, format='png', bbox_inches='tight')
-        #plt.close()
-        img_b64 = base64.b64encode(buf.getvalue()).decode()
-        plt.clf()
-        plt.cla()
-        context= {'data':'Neural Network Accuracy Graph', 'img': img_b64}
-        return render(request, 'UserScreen.html', context)
 
-def CrimePredict(request):    
-    if request.method == 'GET':
-        global dataset, states
-        output = '<tr><td><font size="3" color="black">Choose&nbsp;State</td><td><select name="t1">'
-        for i in range(len(states)):
-            output += '<option value="'+states[i]+'">'+states[i]+'</option>'
-        output +="</select></td></tr>"
-        context= {'data1':output}
-        return render(request, 'CrimePredict.html', context)
+    f = open('model/model_history.pckl', 'rb')
+    hist = pickle.load(f)
+    f.close()
+
+    plt.plot(hist['accuracy'])
+    plt.plot(hist['val_accuracy'])
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+
+    img_b64 = base64.b64encode(buf.getvalue()).decode()
+
+    plt.clf()
+
+    context = {'data': 'Accuracy Graph', 'img': img_b64}
+
+    return render(request, 'UserScreen.html', context)
+
+
+# ---------------- CRIME PREDICT ----------------
+
+def CrimePredict(request):
+
+    load_dataset()
+
+    output = '<select name="t1">'
+
+    for s in states:
+        output += '<option value="'+s+'">'+s+'</option>'
+
+    output += "</select>"
+
+    context = {'data1': output}
+
+    return render(request, 'CrimePredict.html', context)
+
 
 def CrimePredictAction(request):
-    if request.method == 'POST':
-        global dataset
-        areaname = request.POST.get('t1', False)
-        crime = dataset.loc[dataset['State'] == areaname]
-        crime = crime['Rape'].ravel()[0]
-        output = "<font size=3 color=blue>Predicted Number of Crimes on Women in "+areaname+" = "+str(crime)+"</font>"
-        context= {'data':output}
-        return render(request, 'UserScreen.html', context)
+
+    load_dataset()
+
+    areaname = request.POST.get('t1')
+
+    crime = dataset.loc[dataset['State'] == areaname]
+
+    crime = crime['Rape'].ravel()[0]
+
+    output = "Crime in "+areaname+" = "+str(crime)
+
+    context = {'data': output}
+
+    return render(request, 'UserScreen.html', context)
+
+
+# ---------------- PANIC ----------------
 
 def Panic(request):
     return render(request, 'Panic.html', {})
 
+
 def PanicAction(request):
-    if request.method == 'POST':
-        global username, email
-        areaname = request.POST.get('t1', False)
-        em = []
-        em.append(email)
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:
-            email_address = 'kaleem202120@gmail.com'
-            email_password = 'xyljzncebdxcubjq'
-            connection.login(email_address, email_password)
-            connection.sendmail(from_addr="kaleem202120@gmail.com", to_addrs=em, msg=username+" sent emergency message from location "+areaname+" to you.")
-        context= {'data':"Emergency message sent to registered E-Mail"}
-        return render(request, 'Panic.html', context)    
 
-def UserLoginAction(request):
-    global username
-    if request.method == 'POST':
-        global username, email
-        status = "none"
-        users = request.POST.get('t1', False)
-        password = request.POST.get('t2', False)
-        con = pymysql.connect(host='127.0.0.1',port = 3306,user = 'root', password = 'root', database = 'safety',charset='utf8')
-        with con:
-            cur = con.cursor()
-            cur.execute("select username,password,email FROM register")
-            rows = cur.fetchall()
-            for row in rows:
-                if row[0] == users and row[1] == password:
-                    email = row[2]
-                    username = users
-                    status = "success"
-                    break
-        if status == 'success':
-            context= {'data':'Welcome '+username}
-            return render(request, "UserScreen.html", context)
-        else:
-            context= {'data':'Invalid username'}
-            return render(request, 'UserLogin.html', context)
+    global username, email
 
-def RegisterAction(request):
-    if request.method == 'POST':
-        global username
-        username = request.POST.get('t1', False)
-        password = request.POST.get('t2', False)
-        contact = request.POST.get('t3', False)
-        email = request.POST.get('t4', False)
-        address = request.POST.get('t5', False)
-               
-        output = "none"
-        con = pymysql.connect(host='127.0.0.1',port = 3306,user = 'root', password = 'root', database = 'safety',charset='utf8')
-        with con:
-            cur = con.cursor()
-            cur.execute("select username FROM register")
-            rows = cur.fetchall()
-            for row in rows:
-                if row[0] == username:
-                    output = username+" Username already exists"
-                    break                
-        if output == "none":
-            db_connection = pymysql.connect(host='127.0.0.1',port = 3306,user = 'root', password = 'root', database = 'safety',charset='utf8')
-            db_cursor = db_connection.cursor()
-            student_sql_query = "INSERT INTO register VALUES('"+username+"','"+password+"','"+contact+"','"+email+"','"+address+"')"
-            db_cursor.execute(student_sql_query)
-            db_connection.commit()
-            print(db_cursor.rowcount, "Record Inserted")
-            if db_cursor.rowcount == 1:
-                output = "Signup process completed. Login to perform Emergency activities"
-        context= {'data':output}
-        return render(request, 'Register.html', context)       
+    areaname = request.POST.get('t1')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:
+        connection.login("test@gmail.com", "password")
+        connection.sendmail(
+            from_addr="test@gmail.com",
+            to_addrs=[email],
+            msg=username + " emergency from " + areaname
+        )
+
+    context = {'data': "Emergency message sent"}
+
+    return render(request, 'Panic.html', context)
+
+
+# ---------------- LOGIN ----------------
 
 def UserLogin(request):
     return render(request, 'UserLogin.html', {})
 
-def index(request):
-    return render(request, 'index.html', {})
+
+def UserLoginAction(request):
+
+    global username, email
+
+    users = request.POST.get('t1')
+    password = request.POST.get('t2')
+
+    con = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='root',
+        database='safety'
+    )
+
+    with con:
+        cur = con.cursor()
+        cur.execute("select username,password,email from register")
+
+        for row in cur.fetchall():
+
+            if row[0] == users and row[1] == password:
+
+                username = users
+                email = row[2]
+
+                return render(request, "UserScreen.html", {'data': 'Welcome'})
+
+
+    return render(request, 'UserLogin.html', {'data': 'Invalid'})
+
+
+# ---------------- REGISTER ----------------
 
 def Register(request):
     return render(request, 'Register.html', {})
 
 
+def RegisterAction(request):
 
-    
+    username = request.POST.get('t1')
+    password = request.POST.get('t2')
+    contact = request.POST.get('t3')
+    email = request.POST.get('t4')
+    address = request.POST.get('t5')
 
+    con = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='root',
+        database='safety'
+    )
+
+    with con:
+        cur = con.cursor()
+
+        cur.execute(
+            "INSERT INTO register VALUES(%s,%s,%s,%s,%s)",
+            (username, password, contact, email, address)
+        )
+
+    return render(request, 'Register.html', {'data': 'Registered'})
+
+
+# ---------------- INDEX ----------------
+
+def index(request):
+    return render(request, 'index.html', {})
